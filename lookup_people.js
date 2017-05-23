6349fd5f4d34e10cbdc43bf97e6d396e216efc5f
Dong Hu <cdhu@uwaterloo.ca>
2017-05-23 18:36:24 +0000

    Included non-case-sensitive inputs

--- lookup_people.js
+++ lookup_people.js
@@ -6,23 +6,27 @@ const settings = require("./settings"); // settings.json
 // client will read connection information from the same environment variables
 // used by postgres cli tools
 const client = new pg.Client({
-  user     : settings.user,
-  password : settings.password,
-  database : settings.database,
-  host     : settings.hostname,
-  port     : settings.port,
-  ssl      : settings.ssl
+  user: settings.user,
+  password: settings.password,
+  database: settings.database,
+  host: settings.hostname,
+  port: settings.port,
+  ssl: settings.ssl
 });
 
-const input = process.argv[2];
+let input = process.argv[2];
+input = input.charAt(0).toUpperCase() + input.slice(1);
 
 // connect to database
 client.connect((err) => {
   if (err) {
-    throw err;
+    return console.error("Connection Error", err);
   }
   console.log('Searching...');
-  client.query("SELECT * FROM famous_people WHERE last_name ILIKE $1 OR first_name ILIKE $1;", [`${input}%`], function (err, results) {
+  client.query(`select *
+    from famous_people
+    where last_name = $1 OR
+    first_name = $1`, [input], function(err, results) {
     if (err) {
       throw err;
     }
@@ -30,7 +34,9 @@ client.connect((err) => {
   });
 });
 
-outputData = (input, results) => {
-  console.log(`Found ${input.split(' ').length} person(s) by the name ${input}:
-  - ${results.rows[0].id}: ${results.rows[0].first_name} ${results.rows[0].last_name}, born '${results.rows[0].birthdate.getFullYear()}-${results.rows[0].birthdate.getMonth()}-${results.rows[0].birthdate.getDate()}'`);
+function outputData(input, results) {
+  console.log(`Found ${results.rows.length} person(s) by the name '${input}':`);
+  results.rows.forEach((row) => {
+    console.log(`- ${row.id}: ${row.first_name} ${row.last_name}, born '${row.birthdate.getFullYear()}-${row.birthdate.getMonth()}-${row.birthdate.getDate()}'`);
+  });
 }
